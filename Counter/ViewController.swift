4bcaea61b246c6ede2233b1cbae4d991677c80d1
Aleksandr <Al3ksandr213@yandex.ru>
2024-08-15 15:01:14 +0300

    Приложение счётчик создано,отдаю на проверку

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var counterNum: Int = 0
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var counter: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func pressButt() {
+       
+        if counterNum < Int.max {
+            
+            counterNum += 1
+            counter.text = "Значение счётчика: \(counterNum)"
+            
+        } else {
+            
+            counter.text = "Значение счётчика: \(counterNum)"
+            
+        }
+        
+    }
+    
 }
 
