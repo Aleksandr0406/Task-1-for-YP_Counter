7ae10f19cbdac043e5b749d83d6aa55251ebdff0
Aleksandr <Al3ksandr213@yandex.ru>
2024-08-22 11:13:25 +0300

    Добавлена кнопка минус, кнопка обнуление и история изменений

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,29 +8,73 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    var counterNum: Int = 0
-    @IBOutlet weak var button: UIButton!
-    @IBOutlet weak var counter: UILabel!
+    
+    let date = DateFormatter()
+    private var counterNum: Int = 0
+    @IBOutlet private weak var buttonMinus: UIButton!
+    @IBOutlet private weak var buttonPlus: UIButton!
+    @IBOutlet private weak var counter: UILabel!
+    @IBOutlet private weak var history: UITextView!
+    @IBOutlet private weak var null: UIButton!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-    @IBAction func pressButt() {
-       
+    
+    @IBAction private func pressButtPlus() {
+        
+        date.dateFormat = "dd.MM.yyyy, HH:mm:ss"
+        
         if counterNum < Int.max {
-            
             counterNum += 1
             counter.text = "Значение счётчика: \(counterNum)"
+            history.text += "\n\(date.string(from: Date())): значение изменено на +1"
+                
+            } else {
+                
+                counter.text = "Значение счётчика: \(counterNum)"
+                
+            }
+        
+    }
+    
+    @IBAction private func pressButtMinus() {
+        
+        date.dateFormat = "dd.MM.yyyy, HH:mm:ss"
+        
+        if counterNum == 0 {
+            
+            counter.text = "\(counterNum)"
+            history.text += "\n\(date.string(from: Date())): попытка уменьшить значение счётчика ниже 0"
             
         } else {
             
-            counter.text = "Значение счётчика: \(counterNum)"
+            if counterNum == 1 {
+                
+                counterNum -= 1
+                counter.text = "\(counterNum)"
+                history.text += "\n\(date.string(from: Date())): значение изменено на -1"
+                
+            } else {
+                
+                counterNum -= 1
+                counter.text = "Значение счётчика: \(counterNum)"
+                history.text += "\n\(date.string(from: Date())): значение изменено на -1"
+                
+            }
+        }
+    }
+        
+    @IBAction private func makeNull() {
             
+        date.dateFormat = "dd.MM.yyyy, HH:mm:ss"
+        counterNum = 0
+        counter.text = "\(counterNum)"
+        history.text += "\n\(date.string(from: Date())): значение сброшено"
+        
         }
         
     }
     
-}
-
